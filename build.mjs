d5e6debc1b8687de106abc40c746ed4d453a862c
Seva Zaikov <seva.zaikov@gmail.com>
2025-09-27 20:32:30 -0700

    improve build

--- build.mjs
+++ build.mjs
@@ -1,27 +1,22 @@
 import * as esbuild from "esbuild";
-import { writeFile, readFile } from "node:fs/promises";
+import { writeFile, readFile, rm } from "node:fs/promises";
 import * as path from "node:path";
 
 import { fileURLToPath } from "url";
 
+const CHROME_DIR = "chrome";
+const FIREFOX_DIR = "firefox";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// duplication, but it is fast enough not to worry about it
-await esbuild.build({
-  entryPoints: ["src/main.ts"],
-  bundle: true,
-  outdir: "dist/chrome",
-});
+await cleanDistDirectory();
 
-await esbuild.build({
-  entryPoints: ["src/main.ts"],
-  bundle: true,
-  outdir: "dist/firefox",
-});
+await buildTypeScriptFiles(CHROME_DIR);
+await buildTypeScriptFiles(FIREFOX_DIR);
 
-await writeManifest("chrome");
-await writeManifest("firefox");
+await writeManifest(CHROME_DIR);
+await writeManifest(FIREFOX_DIR);
 
 /**
  * Chrome and Firefox manifests are slightly different, so we generate
@@ -34,7 +29,7 @@ async function writeManifest(type) {
     await readFile(new URL(manifestPath, import.meta.url))
   );
 
-  if (type === "chrome") {
+  if (type === CHROME_DIR) {
     delete manifest.background.scripts;
   } else {
     delete manifest.background.service_worker;
@@ -44,4 +39,16 @@ async function writeManifest(type) {
     path.join(__dirname, "dist", type, "manifest.json"),
     JSON.stringify(manifest, null, 2)
   );
+}
+
+function cleanDistDirectory() {
+  return rm(path.join(__dirname, "dist"), { recursive: true, force: true });
+}
+
+function buildTypeScriptFiles(type) {
+  return esbuild.build({
+    entryPoints: ["src/main.ts"],
+    bundle: true,
+    outdir: `dist/${type}`,
+  });
 }
